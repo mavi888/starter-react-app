fae0129cee27a93785927000bb0806536d968103
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2019-01-23 15:43:26 -0300

    Starter app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link, withRouter } from 'react-router-dom';
+
+import { Navbar } from 'react-bootstrap';
+import Routes from './Routes';
+
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App container'>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/'>Twitterino</Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        <div className='lander'>
+          <h1>Twitterino</h1>
+          <p>A simple twitter app</p>
+        </div>
+      </div>
+    );
+  }
+}
